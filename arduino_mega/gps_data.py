825497d597716bf308e705dc879bf725a59b3a2e
uibeom0210 <wjddmlqja17@gmail.com>
2023-03-21 18:32:32 +0900

    Fix gps_data.py
    1. Fix save file path.
    2. Rename time value to gpstime
    3. Add value datetime between sequence and gpstime
    4. Rename save data file name

--- arduino_mega/gps_data.py
+++ arduino_mega/gps_data.py
@@ -1,29 +1,35 @@
 import serial
 import datetime
 
-ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)  # /dev/ttyACM0에서 9600으로 시리얼 포트 열기.
-count = 1
-
+ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1) #communication
+count = 0
+file_count = 0
+file_name = ''
 now = datetime.datetime.now()
-target_time = now + datetime.timedelta(minutes=5)
-f= open(f"/home/pi/gps_data{now.hour}-{now.minute}-{now.second}.csv",'w') # 파일 만들고 쓰기
-f.write("sequence,time,latitude,longitude,speed\n")
-while True:
-    now = datetime.datetime.now()
-    rx = ser.readline().decode()
-    data_type = rx.split(']')
-    if(data_type[0] == "[GPS"):
-        print(count, data_type[-1])
-        f.write(str(count) + ',')
-        f.write(data_type[-1])
-        count = count + 1
-    if now >= target_time:
-        f.close()
-        f= open(f"/home/pi/gps_data{now.hour}-{now.minute}-{now.second}.csv",'w')
-        f.write("sequence,time,latitude,longitude,speed\n")        
-        target_time = now + datetime.timedelta(minutes=5)
-    
-     #if 라즈베리 젯슨 통신 값    
-f.close() #파일 닫기
-ser.close()   # 시리얼 통신 종료
+target_time = now + datetime.timedelta(minutes=2)
+file_name = f"/home/pi/Desktop/GPS/GPS_{now.hour:02d}{now.minute:02d}{now.second:02d}.csv" #used datetime
+f= open(file_name,'w')
+f.write("sequence,datetime,gpstime,latitude,longitude,speed\n") #Save 5 value
+f.close()
 
+while True:
+    rx = ser.readline().decode()
+    data_type = rx.split(']') #parsing
+    if(data_type[0] == "[GPS"):
+        f= open(file_name,'a')
+        f.write(f"{count},{now.hour:02d}{now.minute:02d}{now.second:02d},")
+        f.write(data_type[-1])
+        f.close()
+        count = count + 1
+    if now >= target_time: #if 2minute later
+        file_count += 1
+        count = 0
+        if file_count == 10: #if file_count 10 finish
+            break
+        file_name = f"/home/pi/Desktop/GPS/GPS_{now.hour:02d}{now.minute:02d}{now.second:02d}.csv" #repeat
+        f= open(file_name,'w')
+        f.write("sequence,datetime,gpstime,latitude,longitude,speed\n")
+        f.close()
+        target_time = now + datetime.timedelta(minutes=2)
+
+ser.close()
