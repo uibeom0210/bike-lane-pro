156c9721a5ac2600f08d350e8a1bc836ba857c31
Lee Hyun Gyu <acecm1030@naver.com>
2023-03-28 13:30:51 +0900

    Fix gps date and time

--- arduino_mega/gps_mega/gps_mega.ino
+++ arduino_mega/gps_mega/gps_mega.ino
@@ -1,48 +1,53 @@
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
-#define GPSBAUD 9600 //gps 통신
+#define GPSBAUD 9600  //gps 통신
 
 TinyGPS gps;
-void getgps(TinyGPS &gps);
+void getgps(TinyGPS &gps); //gps 함수 선언
 
-void setup() {
-  Serial.begin(9600);
-  Serial1.begin(GPSBAUD);
-	gps = TinyGPS();
+void setup() 
+{
+  Serial.begin(9600); //시리얼 통신 사용(라즈베리)
+  Serial1.begin(GPSBAUD); //시리얼 통신 사용(GPS)
+  gps = TinyGPS(); 
 }
 
-void loop() {
-  while(Serial1.available()) 
+void loop() 
+{
+  while (Serial1.available()) //GPS 통신
   {
-    int gpsCode = Serial1.read();
-    if(gps.encode(gpsCode))
+    int gpsCode = Serial1.read(); 
+    if (gps.encode(gpsCode)) 
     {
-      getgps(gps);
+      getgps(gps); //함수 실행
     }
   }
 }
 
-void getgps(TinyGPS &gps)
+void getgps(TinyGPS &gps) 
 {
- Serial.print("[GPS][DEV0] ");
-  int year; 
-  byte month, day, hour, minute, second;
-  gps.crack_datetime(&year,&month,&day,&hour,&minute,&second);
-  Serial.print(year);Serial.print("/");
-  Serial.print(month, DEC);Serial.print("/");
-  Serial.print(day, DEC);Serial.print("/");
-  Serial.print(hour+9, DEC);Serial.print("/");
-  Serial.print(minute, DEC);Serial.print("/");Serial.print(second, DEC);
+  char buf[11]; //GPS 값을 저장할 변수
+  Serial.print("[GPS][DEV0]"); //팀 프로토콜에 맞춰 GPS데이터 임을 알린다
+  int year;
+  byte month, day, hour, minute, second; //날짜와 시간 데이터 변수 선언
+  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second); 
+  sprintf(buf, "%02d", hour+9); Serial.print(buf); //시간 출력
+  sprintf(buf, "%02d", minute); Serial.print(buf); //분 출력
+  sprintf(buf, "%02d", second); Serial.print(buf); // 초 출력
+  // Serial.print(hour + 9, DEC); 
+  // Serial.print(minute, DEC);
+  // Serial.print(second, DEC);
   Serial.print(",");
 
-  gps.f_get_position(&latitude, &longitude);
-  Serial.print(latitude,5);
+  float latitude, longitude; //위도 경도 변수 선언
+  gps.f_get_position(&latitude, &longitude); //위도 경도 값 확인
+  Serial.print(latitude,8); //소수점 아래 8자리까지 출력
   Serial.print(",");
-  Serial.print(longitude,5);
+  Serial.print(longitude,8);
   Serial.print(",");
-  Serial.println(gps.f_speed_kmph());
-  
+  Serial.println(gps.f_speed_kmph()); //속도 출력
+
   unsigned long chars;
   unsigned short sentences, failed_checksum;
-  gps.stats(&chars,&sentences,&failed_checksum);
+  gps.stats(&chars, &sentences, &failed_checksum); //데이터 통계 및 각 변수에 저장합니다.
 }
