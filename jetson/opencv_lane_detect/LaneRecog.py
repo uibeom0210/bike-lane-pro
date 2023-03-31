bea1a137a8febba94bbfc6767d075520d3e9bf0d
JMlee98 <122666170+JMyoung98@users.noreply.github.com>
2023-03-31 10:11:44 +0200

    Feature/cam (#19)
    * Add_same_code_for_window
    
    * weight
    
    * add annotation
    
    * pothole and manhole train model
    
    * trainin_code_by_colab
    
    * Add for linux
    
    * The last code for project
    
    It have python code, script, and yolov5 model(pothole_model.pt)
    
    * Delete add_yolo_custom1.py
    
    * Delete add_yolo_custom_for_window.py
    
    * Delete final3.pt
    
    * Delete final_pothole_training4.pt
    
    * Delete lane_detect.py
    
    * Update add_yolo_custom_with_Linux.py

--- jetson/opencv_lane_detect/LaneRecog.py
+++ jetson/opencv_lane_detect/LaneRecog.py
@@ -1,18 +1,8 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-
-# 캐니 에지 디텍션
-def Canny(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
-    canny = cv2.Canny(blur, 50, 200)
-    cv2.imshow('canny',canny)
-    return canny
-
+#연산영역을 줄이기 위한 ROI 설정
 def InterestRegion(frame, width, height):
-    #Width 기준
     frame = np.array(frame, dtype=np.uint8)
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower_white = np.array([0,0,0])
@@ -28,47 +18,3 @@ def InterestRegion(frame, width, height):
     interestarea = cv2.bitwise_and(res, mask) # 원본 이미지와 교집합 구하기
     
     return interestarea
-
-
-
-def Hough_lines(interestregion, rho, theta, threshold, min_line_len, max_line_gap):
-    lines = cv2.HoughLinesP(interestregion,rho,theta,threshold,np.array([]),minLineLength=min_line_len, maxLineGap=max_line_gap)
-    return lines
-
-def weighted_img(img, initial_img, a=1, b=1., l=0.): # 두 이미지 operlap 하기
-    return cv2.addWeighted(initial_img, a, img, b, l)
-
-def draw_lines(frame, lines, color=[0, 0, 255], thickness=2): # 선 그리기
-    cv2.line(frame, (lines[0], lines[1]), (lines[2], lines[3]), color, thickness=0.3)
-
-
-def get_representative(frame, lines):  # Representative Line 구하기
-    lines = np.squeeze(lines)
-    lines = lines.reshape(lines.shape[0] * 2, 2)
-    rows, cols = frame.shape[:2]
-    output = cv2.fitLine(lines, cv2.DIST_L2, 0, 0.01, 0.01)
-    vx, vy, x, y = output[0], output[1], output[2], output[3]
-    x1, y1 = int(((frame.shape[0] - 1) - y) / vy * vx + x), frame.shape[0] - 1
-    x2, y2 = int(((frame.shape[0] / 2 + 100) - y) / vy * vx + x), int(frame.shape[0] / 2 + 100)
-
-    result = [x1, y1, x2, y2]
-    return result
-
-def top_view(frame, width, height):
-    #area = np.array([[(width*0.5,(height*0.4)),(0,(height*0.65)),(0,height), (width,height),(width,(height*0.6))]], np.int32) # Area 지정
-    #좌상, 좌하, 우상, 우하
-    '''
-    cv2.circle(frame, (int(width*0.2), int(height*0.6)), 5, (255, 255, 255), -1)
-    cv2.circle(frame, (int(width * 0.7), int(height * 0.6)), 5, (255, 255, 255), -1)
-    '''
-    left_bottom = [0,height*0.1]
-    right_bottom = [width,height*0.1]
-    left_top = [int(width*0.2),int(height*0.55)]
-    right_top = [int(width*0.8), int(height*0.55)]
-    pts1 = np.float32([[left_top,left_bottom,right_top,right_bottom]])
-    # 좌표의 이동점
-    pts2 = np.float32([[0, 0], [0, 480], [640, 0], [640, 480]])
-    # pts1의 좌표에 표시. perspective 변환 후 이동 점 확인.
-    M = cv2.getPerspectiveTransform(pts1, pts2)
-    dst = cv2.warpPerspective(frame, M, (640, 480))
-    return dst
--- jetson/opencv_lane_detect/video_record.py
+++ jetson/opencv_lane_detect/video_record.py
@@ -1,52 +1,52 @@
 import cv2
-import datetime as dt
+from datetime import datetime
 
+# 웹캠 캡쳐 객체 생성
+video = cv2.VideoCapture(0, cv2.CAP_V4L2)
+if not video.isOpened(): raise Exception("error")
 
-try:
-    video = cv2.VideoCapture(0, cv2.CAP_V4L2)
-    if not video.isOpened(): raise Exception("error")
-    date = dt.datetime.strftime('%H%M%S')
-    
-    fps = video.get(cv2.CAP_PROP_FPS)
-    delay = int(1000/fps)
-    frame_cnt = 1
-    width = 640
-    height = 480
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    i=1
-    cnt=0
-    out = cv2.VideoWriter(f'/home/jetson/Desktop/CAM{date}.avi',fourcc,fps,(int(width),int(height)))
-except Exception as e:
-    print("Exception occurred:", e)
-           
+#변수 설정
+fps = video.get(cv2.CAP_PROP_FPS)
+frame_cnt = 1
+width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+i=0
+pre_time = datetime.now()
+pre_time_name = pre_time.strftime('%H%M%S')
+
+#비디오 파일 저장을 위한 루프
 while True:
-    try:
-        cnt = 0
-        while(video.isOpened()):
-            
-            ret, frames = video.read()
-            if not ret: break
-            cv2.imshow('original',frames)
-            out.write(frames)
+    out = cv2.VideoWriter(f'/home/jetson/Desktop/CAM/CAM_{pre_time_name}.avi',fourcc,fps,(int(width),int(height)))
+    cnt = 0
+    # 웹캠 프레임 읽기 루프
+    while(video.isOpened()):
 
-            cnt = cnt+1
-            #print(cnt)
-            if(cnt == int(fps*120) or (cv2.waitKey(1)&0xff==ord('q'))):
-                date = dt.datetime.now()
-                i = i +1
-                break
-        if 'out' in locals():
-            
-            out.release()
-            
-        if(i == 11): break
-        out = cv2.VideoWriter(f'/home/jetson/Desktop/CAM{date}.avi',fourcc,fps,(int(width),int(height)))
-    except Exception as e:
-        print("Exception occurred:", e)
-        break
-try:
-    video.release()
-    cv2.destroyAllWindows()
-except Exception as e:
-    print("Exception occurred:", e)
+        ret, frames = video.read()
+        if not ret: break
         
+        cv2.imshow('original',frames)
+        out.write(frames)
+        cur_time = datetime.now()
+        
+        #이전 시간과 현재 시간의 차이가 2분이라면 루프문 탈출
+        if (cur_time.minute - pre_time.minute >= 2):
+            pre_time = cur_time
+            pre_time_name = pre_time.strftime('%H%M%S')
+            i = i +1
+            break
+
+        # q를 누를시 강제로 루프문 탈출
+        if cv2.waitKey(1)&0xff==ord('q') :
+            i = 10
+            break
+
+    #동영상파일이 있다면 조건문 실행 
+    if 'out' in locals():
+        out.release()
+    
+    #10개의 파일이 만들어지면 루프문 탈출
+    if(i == 10): break
+
+video.release()
+cv2.destroyAllWindows()
